0b890f17451b996009dcaa3dbcaceea321ac28b3
solar-rays <solarinayomide08@gmail.com>
2025-05-21 07:56:49 +0100

    minor bug fixes

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,17 +1,17 @@
 import './sidebar.css'
 import image1 from './Assets/Image_1.jpg'
+import image3 from './Assets/Image_3.jpg'
 
 function Sidebar(props) {
     return (
         <div className='sidebar'>
             <div className='profile-container'>
-                <img src={image1} alt="profile"/>
+                <img src={image3} alt="profile"/>
                 <div className="profile-info"><p>Name: </p><span>{props.name}</span></div>
                 <div className="profile-info"><p>Age: </p><span>{props.age}</span></div>
                 <div className="profile-info"><p>Gender: </p><span>{props.gender}</span></div>
                 <div className="profile-info"><p>Height: </p><span>{props.height}</span></div>
             </div>
-            <div></div>
         </div>
     )
 }
